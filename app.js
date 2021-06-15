26533c4e1fbbc38e668fd53c30761d0536875f26
albertthewinner2000 <nalbert2012@ya.ru>
2021-06-15 20:18:49 +0300

    init: initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '1865790075:AAEbXH1p4-KxXY1jPStSWMB-H1Lg05_8OwE';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {
+    polling: true
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; 
+
+    bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/showLatest/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    
+    bot.sendMessage(chatId, 'latest ads');
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+});
